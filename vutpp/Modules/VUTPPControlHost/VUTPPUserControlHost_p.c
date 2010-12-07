f5ec6ff100043c73704d0226130d693e01a31c9f
powerumc <powerumc@0b7a03c8-c844-0410-8ba4-158efae20018>
2010-12-07 16:06:41 +0000

    Support for VS2010

--- vutpp/Modules/VUTPPControlHost/VUTPPUserControlHost_p.c
+++ vutpp/Modules/VUTPPControlHost/VUTPPUserControlHost_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Dec 08 00:15:14 2010
+/* at Wed Dec 08 01:02:38 2010
  */
 /* Compiler settings for VUTPPUserControlHost.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
